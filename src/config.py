16588baed39b8011f14e4dd5d2fb7309a05cbe2e
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-09-22 14:04:39 +1000

    feat: MAAHG2 CLI

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ import os
 
 INTERFACE_PATH = "config/interface.json"
 CONFIG_PATH = "config/config.json"
+RESOURCE_PATH = "assets/resource/mix/pipeline/Event.json"
 
 def load_json(json_path):
     if not os.path.exists(json_path):
@@ -15,6 +16,12 @@ def load_json(json_path):
 class config_client():
     def __init__(self, interface_path=INTERFACE_PATH, config_path=CONFIG_PATH) -> None:
         self.interface_path = interface_path
+        self.load_interface(self.load_interface)
+
+        self.config_path = config_path
+        self.load_config(self.config_path)
+
+    def load_interface(self, interface_path):
         self.data = load_json(self.interface_path)['client']
         self.client_name = []
         self.client_path = []
@@ -24,8 +31,8 @@ class config_client():
             self.client_path.append(client['path'])
             self.indexes.append(index)
 
-        self.config_path = config_path
-        self.active = load_json(self.config_path)
+    def load_config(self, config_path):
+        self.config = load_json(self.config_path)
 
     def get_clients_info(self):
         return self.client_name, self.client_path, self.indexes
@@ -58,19 +65,18 @@ class config_client():
             return "index out of range"
 
     def get_active_client_info(self):
-        active_name = self.active['client']
-        active_path = self.active['resource_path']
+        active_name = self.config['client']
+        active_path = self.config['resource_path']
         return active_name, active_path
 
     def set_active_client(self, index):
         try:
             name = self.get_client_name(index)
             path = self.get_client_path(index)
-            self.active['client'] = name
-            self.active['resource_path'] = path
+            self.config['client'] = name
+            self.config['resource_path'] = path
             with open(self.config_path, 'w', encoding='utf-8') as f:
-                json.dump(self.active, f, indent=4)
-            self.active = load_json(self.config_path)
+                json.dump(self.config, f, ensure_ascii=False, indent=4)
             return f"Client setted to {self.get_active_client_info()}"
         except:
             return "index out of range"
@@ -78,6 +84,12 @@ class config_client():
 class config_task():
     def __init__(self, interface_path=INTERFACE_PATH, config_path=CONFIG_PATH) -> None:
         self.interface_path = interface_path
+        self.load_interface(self.load_interface)
+
+        self.config_path = config_path
+        self.load_config(self.config_path)
+
+    def load_interface(self, interface_path):
         self.data = load_json(self.interface_path)['task']
         self.task_name = []
         self.task_entry = []
@@ -87,12 +99,12 @@ class config_task():
             self.task_entry.append(task['entry'])
             self.indexes.append(index)
 
-        self.config_path = config_path
+    def load_config(self, config_path):
         self.active_task = []
         self.active_indexes = []
-        self.active = load_json(self.config_path)
-        for index, task in enumerate(self.active['task'], start=1):
-            self.active_task.append(self.active['task'][task])
+        self.config = load_json(self.config_path)
+        for index, task in enumerate(self.config['task'], start=1):
+            self.active_task.append(self.config['task'][task])
             self.active_indexes.append(index)
 
     def get_task_list(self):
@@ -148,14 +160,17 @@ class config_task():
         return f"{task_to_add['name']} is added"
 
     def remove_task(self, index):
-        index = index - 1
-        task_to_remove = self.active_task.pop(index)
-        self.set_active_task()
-        return f"{task_to_remove['name']} is removed"
+        try:
+            index = index - 1
+            task_to_remove = self.active_task.pop(index)
+            self.set_active_task()
+            return f"{task_to_remove['name']} is removed"
+        except:
+            return f"out of index"
 
     def move_task(self, index, pos):
-        index = index - 1
-        pos = pos - 1
+        index = int(index) - 1
+        pos = int(pos) - 1
         task_to_move = self.active_task.pop(index)
         self.active_task.insert(pos, task_to_move)
         self.set_active_task()
@@ -163,6 +178,65 @@ class config_task():
 
     def set_active_task(self):
         new_active_task = {str(self.active_indexes + 1): task for self.active_indexes, task in enumerate(self.active_task)}
-        self.active['task'] = new_active_task
+        self.config['task'] = new_active_task
         with open(self.config_path, 'w', encoding='utf-8') as f:
-            json.dump(self.active, f, indent=4)
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+        return True
+
+class config_event():
+    def __init__(self, config_path=CONFIG_PATH, resource_path=RESOURCE_PATH) -> None:
+        self.config_path = config_path
+        self.load_config(self.config_path)
+
+        self.resource_path = resource_path
+        self.load_resource(self.resource_path)
+
+    def load_config(self, config_path):
+        self.config = load_json(self.config_path)
+        self.event_name = self.config['event']['name']
+        self.event_time = self.config['event']['time']
+
+    def load_resource(self, resource_path):
+        self.resource = load_json(self.resource_path)
+        self.find_event = self.resource['FindEvent_inverse']
+        self.enter_event = self.resource['EnterEvent']
+
+    def get_event(self):
+        return self.event_name, self.event_time
+
+    def get_event_name(self):
+        return self.event_name
+
+    def get_event_time(self):
+        return self.event_time
+
+    def get_event_name_resource(self):
+        return self.enter_event['expected'][0]
+
+    def config_event(self, name, time=3):
+        self.config['event']['name'] = name
+        self.config['event']['time'] = time
+        with open(self.config_path, 'w', encoding='utf-8') as f:
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+        return True
+
+    def config_event_name(self, name) -> bool:
+        self.config['event']['name'] = name
+        with open(self.config_path, 'w', encoding='utf-8') as f:
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+        return True
+
+    def config_event_time(self, time=3) -> bool:
+        self.config['event']['time'] = time
+        with open(self.config_path, 'w', encoding='utf-8') as f:
+            json.dump(self.config, f, ensure_ascii=False, indent=4)
+        return True
+
+    def set_event(self, name):
+        self.find_event['expected'] = [name]
+        self.enter_event['expected'] = [name]
+        self.resource['FindEvent_inverse'] = self.find_event
+        self.resource['EnterEvent'] = self.enter_event
+        with open(self.resource_path, 'w', encoding='utf-8') as f:
+            json.dump(self.resource, f, ensure_ascii=False, indent=4)
+        return True
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from config import config_client, config_task
+from config import config_client, config_task, config_event
 
 from maa.context import SyncContext
 from maa.define import RectType
@@ -9,72 +9,194 @@ from maa.toolkit import Toolkit
 from maa.custom_action import CustomAction
 
 import asyncio
+import os
 
-async def main():
-    # Initial Toolkit
-    user_path = "./"
-    Toolkit.init_option(user_path)
-
-    # Connect ADB device
-    device_list = await Toolkit.adb_devices()
-    if not device_list:
-        print("No ADB device found.")
-        input("Press any key to exit...")
-        exit()
-    if len(device_list) == 1:
-        device = device_list[0]
-    else :
-        print("Choose devices: ")
-        device_index = 1
-        for device in device_list:
-            print(f"{device_index}. {device.address}")
-            device_index += 1
-        device_num = input(f"Please input [1-{len(device_list)}]: ")
-        while int(device_num) not in range(1,len(device_list)+1):
-            device_num = input(f"Invalid value, please input [1-{len(device_list)}]: ")
-        device = device_list[int(device_num)-1]
-    print(f"Connecting to controller: {device.address}...")
-    controller = AdbController(
-        adb_path=device.adb_path,
-        address=device.address,
-    )
-    await controller.connect()
-    print("Connected.")
-
+async def start():
     client_config = config_client()
-
-    # Load resource
-    resource = Resource()
-    path = client_config.get_active_client_info()[1]
-    await resource.load(path[0])
-
-    print("Binding resource and controller...")
-    maa_inst = Instance()
-    maa_inst.bind(resource, controller)
-    if not maa_inst.inited:
-        print("Failed to init MAA.")
-        input("Press any key to exit...")
-        exit()
-    print("Binded.")
-
-    maa_inst.register_action("OpenGame", open_game)
-    maa_inst.register_action("EnterGame", enter_game)
-    maa_inst.register_action("StartSocial", start_social)
-    maa_inst.register_action("StartStore", start_store)
-    maa_inst.register_action("StartFracture", start_fracture)
-    maa_inst.register_action("StartEvent", start_event)
-    maa_inst.register_action("StartTask", start_task)
-    maa_inst.register_action("FightStart", fight_start)
-    maa_inst.register_action("FightEnd", fight_end)
-    maa_inst.register_action("CloseGame", Close_game)
-
+    event_config = config_event()
     task_config = config_task()
 
-    print("Start...")
-    entries = task_config.get_active_task_entry()
-    for entry in entries:
-        await maa_inst.run_task(entry)
-    print("Finish")
+    def clear():
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def set_latest_event(event_config = config_event()):
+        event_now = event_config.get_event_name_resource()
+        print(f"The current event: {event_now}")
+        event_to_set = input(f"Set the latest event (leave blank to continue using current activity):\n")
+        if event_to_set:
+            if event_config.config_event_name(event_to_set) and event_config.set_event(event_to_set):
+                print(f"The current event set to: {event_to_set}")
+            else:
+                print(f"Setting failed, continue event {event_now}")
+        return True
+
+    def set_event_battle_time(event_config = config_event()):
+        battle_time_now = event_config.get_event_time()
+        print(f"The current battle times: {battle_time_now}")
+        battle_time_to_set = input(f"Set battle times (leave blank to continue using current battle times):\n")
+        if battle_time_to_set:
+            if event_config.config_event_time(battle_time_to_set):
+                print(f"The current battle times set to: {battle_time_to_set}")
+            else:
+                print(f"Setting failed, continue using battle times: {battle_time_now}")
+        return True
+
+    def add_task(task_config = config_task):
+        clear()
+        print("Current tasks: ")
+        task_list = task_config.get_active_task_name()
+        if task_list:
+            for index, task in enumerate(task_list, start=1):
+                print(f"\t{f'{index}.':4}{task}")
+        print("Add task: ")
+        task_list = task_config.get_task_names()
+        for index, task in enumerate(task_list, start=1):
+            print(f"\t{f'{index}.':4}{task}")
+        add_task_index = int(input(f"Please input [1-{len(task_list)}]: "))
+        task_config.add_task(add_task_index)
+        return True
+
+    def move_task(task_config = config_task):
+        clear()
+        print("Move task: ")
+        task_list = task_config.get_active_task_name()
+        if task_list:
+            for index, task in enumerate(task_list, start=1):
+                print(f"\t{f'{index}.':4}{task}")
+            move_task_index = input(f"From [1-{len(task_list)}]: ")
+            move_task_pos = input(f"To [1-{len(task_list)}]: ")
+            task_config.move_task(move_task_index,move_task_pos)
+        return True
+
+    def delete_task(task_config = config_task()):
+        clear()
+        print("Delete task: ")
+        task_list = task_config.get_active_task_name()
+        if task_list:
+            for index, task in enumerate(task_list, start=1):
+                print(f"\t{f'{index}.':4}{task}")
+            delete_task_index = int(input(f"Please input [1-{len(task_list)}]: "))
+            task_config.remove_task(delete_task_index)
+        return True
+
+    async def main():
+        # Initial Toolkit
+        user_path = "./"
+        Toolkit.init_option(user_path)
+
+        # Connect ADB device
+        device_list = await Toolkit.adb_devices()
+        if not device_list:
+            print("No ADB device found.")
+            input("Press any key to exit...")
+            exit()
+        if len(device_list) == 1:
+            device = device_list[0]
+        else :
+            print("Choose devices: ")
+            device_index = 1
+            for device in device_list:
+                print(f"{device_index}. {device.address}")
+                device_index += 1
+            device_num = input(f"Please input [1-{len(device_list)}]: ")
+            while int(device_num) not in range(1,len(device_list)+1):
+                device_num = input(f"Invalid value, please input [1-{len(device_list)}]: ")
+            device = device_list[int(device_num)-1]
+        print(f"Connecting to controller: {device.address}...")
+        controller = AdbController(
+            adb_path=device.adb_path,
+            address=device.address,
+        )
+        await controller.connect()
+        print("Connected.")
+
+        global times_to_fight
+        event_name, times_to_fight = event_config.get_event()
+        event_config.set_event(event_name)
+
+        # Load resource
+        resource = Resource()
+        path = client_config.get_active_client_info()[1]
+        await resource.load(path[0])
+
+        print("Binding resource and controller...")
+        maa_inst = Instance()
+        maa_inst.bind(resource, controller)
+        if not maa_inst.inited:
+            print("Failed to init MAA.")
+            input("Press any key to exit...")
+            exit()
+        print("Binded.")
+
+        maa_inst.register_action("OpenGame", open_game)
+        maa_inst.register_action("EnterGame", enter_game)
+        maa_inst.register_action("StartSocial", start_social)
+        maa_inst.register_action("StartStore", start_store)
+        maa_inst.register_action("StartFracture", start_fracture)
+        maa_inst.register_action("StartEvent", start_event)
+        maa_inst.register_action("StartTask", start_task)
+        maa_inst.register_action("FightStart", fight_start)
+        maa_inst.register_action("FightEnd", fight_end)
+        maa_inst.register_action("CloseGame", Close_game)
+
+        entries = task_config.get_active_task_entry()
+        for entry in entries:
+            await maa_inst.run_task(entry)
+        print("Finish")
+        return True
+
+    async def menu():
+        while 1:
+            clear()
+            # Show current client
+            print("Current client:")
+            client_name, _ = client_config.get_active_client_info()
+            print(f"\t{client_name}")
+
+            # Show current tasks
+            print("Tasks:")
+            task_list = task_config.get_active_task_name()
+            for index, task in enumerate(task_list, start=1):
+                print(f"\t{f'{index}.':4}{task}")
+            event_flag = 1 if 'StartEvent' in \
+                task_config.get_active_task_entry() else 0
+
+            # Show current event (if event task included)
+            if event_flag:
+                print("Current event:")
+                print(f"\t{'Event name:':14}{event_config.get_event_name_resource()}")
+                print(f"\t{'Battle times:':14}{event_config.get_event_time()}")
+
+            # Select action
+            print("### Select action ###")
+            if event_flag:
+                print(f"\t0. 配置活动设置| Config event")
+            print(f"\t1. Add task")
+            print(f"\t2. Move task")
+            print(f"\t3. Delete task")
+            print(f"\t4. Run task")
+            print(f"\t5. Exit")
+            action_index = int(input(f"Please input [{0 if event_flag else 1}-5]: "))
+            while action_index not in range(0 if event_flag else 1,5+1):
+                action_index = int(input(f"Invalid value, please input [{0 if event_flag else 1}-5]: "))
+
+            if action_index == 0:
+                clear()
+                set_latest_event(event_config)
+                set_event_battle_time(event_config)
+            elif action_index == 1:
+                add_task(task_config)
+            elif action_index == 2:
+                move_task(task_config)
+            elif action_index == 3:
+                delete_task(task_config)
+            elif action_index == 4:
+                await main()
+            elif action_index == 5:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                exit()
+
+    await menu()
 
 class OpenGame(CustomAction):
     def run(self, context, task_name, custom_param, box, rec_detail) -> bool:
@@ -120,6 +242,11 @@ class EnterGame(CustomAction):
         print("SkipAnnouncement")
         SkipAnnouncement = context.run_task("SkipAnnouncement")
         print(SkipAnnouncement)
+
+        while SkipAnnouncement:
+            print("SkipAnnouncement")
+            SkipAnnouncement = context.run_task("SkipAnnouncement")
+            print(SkipAnnouncement)
 
         print("WaitForOtherTask")
         WaitForOtherTask = context.run_task("WaitForOtherTask")
@@ -382,13 +509,11 @@ class StartEvent(CustomAction):
         ViewBuddyTable = context.run_task("ViewBuddyTable")
         print(ViewBuddyTable)
 
-        times_to_fight = 3
-
         self.fighting(context)
 
         print("Fight times: 1")
 
-        for times_fight in range(times_to_fight - 1):
+        for times_fight in range(int(times_to_fight) - 1):
             if using_double:
                 print("UsingDouble")
                 UsingDouble = context.run_task("UsingDouble")
@@ -573,4 +698,6 @@ fight_start = FightStart()
 fight_end = FightEnd()
 Close_game = CloseGame()
 
-asyncio.run(main())
+times_to_fight = 1
+
+asyncio.run(start())
