7e688fb0bfd948b8b750a0dd03ec7ada5a58dbb9
Caikun Peng <148462372+Caikun-Peng@users.noreply.github.com>
2024-09-22 16:23:51 +1000

    perf: GitHub Action workflow

--- build.py
+++ build.py
@@ -43,3 +43,41 @@ PyInstaller.__main__.run([
     f'--add-data={add_data_param2}',
     '--clean',
 ])
+
+import shutil
+
+source_path = './dist/MAAHG2.exe'
+destination_path = './'
+
+try:
+    shutil.move(source_path, destination_path)
+    print(f"File moved successfully from {source_path} to {destination_path}")
+except FileNotFoundError:
+    print("The source or destination path does not exist")
+except PermissionError:
+    print("You do not have permission to move the file")
+except Exception as e:
+    print("Error occurred while moving the file:", e)
+
+def remove_file(file_path):
+    try:
+        os.remove(file_path)  # 或者使用 os.unlink(file_path)
+        print("File removed successfully")
+    except FileNotFoundError:
+        print("File does not exist")
+    except PermissionError:
+        print("You do not have permission to delete this file")
+    except Exception as e:
+        print("Error occurred:", e)
+
+def remove_folder(folder_path):
+    try:
+        shutil.rmtree(folder_path)
+        print("Folder and all its contents removed successfully")
+    except Exception as e:
+        print("Error occurred while deleting folder:", e)
+
+remove_folder("build")
+remove_folder("dist")
+
+remove_file("MAAHG2.spec")
